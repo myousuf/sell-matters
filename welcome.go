87ac71c839391bad698506668d3b830ff75fb1ab
Yousuf Qureshi <yousuf.qureshi@gmail.com>
2015-05-04 12:01:51 +0400

    make it better

--- welcome.go
+++ welcome.go
@@ -1,14 +1,37 @@
 package main
 
 import (
-        "fmt"
+       "fmt"
         "net/http"
 )
 
 func init() {
-        http.HandleFunc("/", handler)
+        //http.HandleFunc("/", handler)
+ 		
+ 		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+ 			//fmt.Fprint(w,r.URL.Path[1:])
+        	http.ServeFile(w, r, r.URL.Path[1:])
+    	})        
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
+
         fmt.Fprint(w, "Welcome from the language of the coming decade - Go!")
+		//http.Handle("/", http.FileServer(http.Dir("./static")))
+        //http.ListenAndServe(":3000", nil)
 }
+
+// func main() {
+//     http.HandleFunc("/", homeHandler)
+//     http.HandleFunc("/static/", func(w http.ResponseWriter, r *http.Request) {
+//         http.ServeFile(w, r, r.URL.Path[1:])
+//     })
+//     //panic(http.ListenAndServe(":8080", nil))
+//     http.ListenAndServe(":8080", nil)
+// }
+
+// func homeHandler(w http.ResponseWriter, r *http.Request) {
+	
+// 	//fmt.Fprint(w, "Welcome from the language of the coming decade - Go!")
+//     fmt.Fprintf(w, "<html><head></head><body><h1>Welcome Home!</h1><a href=\"/static/img/test.gif\">Show Image!</a></body></html>")
+// }
