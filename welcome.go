04997d15d2cf6150e46346b11f084d58cd796d51
Yousuf Qureshi <yousuf.qureshi@gmail.com>
2015-05-04 11:24:44 +0400

    make it better

---
+++ welcome.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+        "fmt"
+        "net/http"
+)
+
+func init() {
+        http.HandleFunc("/", handler)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprint(w, "Welcome from the language of the coming decade - Go!")
+}
