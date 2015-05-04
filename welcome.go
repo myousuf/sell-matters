0cc3e49c55f65e74faf4d63b9d7bbe37f3670ab8
myousuf <yousuf.qureshi@gmail.com>
2015-05-04 23:21:50 +0400

    update file

--- welcome.go
+++ welcome.go
@@ -19,7 +19,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
         http.ServeFile(w, r, r.URL.Path[1:])
         // fmt.Fprint(w, "Welcome from the language of the coming decade - Go!")
 		//http.Handle("/", http.FileServer(http.Dir("./static")))
-        //http.ListenAndServe(":3000", nil)
+        http.ListenAndServe(":8080", nil)
 }
 
 func main() {
