2aee873f05196036cb87db7101ce316b87c8cb14
myousuf <yousuf.qureshi@gmail.com>
2015-05-05 10:14:57 +0400

    modified welcome.go

--- welcome.go
+++ welcome.go
@@ -9,10 +9,10 @@ import (
 func init() {
         //http.HandleFunc("/", handler)
         
-        http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-            //fmt.Fprint(w,r.URL.Path[1:])
-            http.ServeFile(w, r, r.URL.Path[1:])
-        })        
+        // http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        //     //fmt.Fprint(w,r.URL.Path[1:])
+        //     http.ServeFile(w, r, r.URL.Path[1:])
+        // })        
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -20,10 +20,15 @@ func handler(w http.ResponseWriter, r *http.Request) {
         http.ServeFile(w, r, r.URL.Path[1:])
         // fmt.Fprint(w, "Welcome from the language of the coming decade - Go!")
         //http.Handle("/", http.FileServer(http.Dir("./static")))
-        http.ListenAndServe(":8080", nil)
+        //http.ListenAndServe(":8080", nil)
 }
 
 func main() {
+
+            http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+            //fmt.Fprint(w,r.URL.Path[1:])
+            http.ServeFile(w, r, r.URL.Path[1:])
+        }) 
    
     http.ListenAndServe(":8080", nil)
 }
